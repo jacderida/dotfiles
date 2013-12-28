b15b265db6903d6045baff185dc6c0b4c6491b93
Chris O'Neil <chris.oneil@gmail.com>
2013-12-28 20:11:06 +0000

    Add bashrc with powerline enabled
    This is actually the template bashrc that comes with the default
    install. I didn't realise there'd be so much in it. Added the powerline
    binary to the path and configured powerline itself to run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ DOTFILES_ROOT="`pwd`"
 
 for bootstrap in `find $DOTFILES_ROOT -name "bootstrap" -not -path ./*.sh` 
 do
+	echo "================================================================="
 	echo "Running $bootstrap"
+	echo "================================================================="
 	source $bootstrap
 done
