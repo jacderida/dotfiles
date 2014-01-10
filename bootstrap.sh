2b67f7f0f3cee746aa15d6aca4a4f9052655baeb
Chris O'Neil <chris.oneil@gmail.com>
2014-01-10 21:10:01 +0000

    Add colour codes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+black='\033[0;30m'
+blue='\033[0;34m'
+green='\033[0;32m'
+cyan='\033[0;36m'
+red='\033[0;31m'
+purple='\033[0;35m'
+orange='\033[0;33m'
+light_gray='\033[0;37m'
+dark_gray='\033[1;30m'
+light_blue='\033[1;34m'
+light_green='\033[1;32m'
+light_cyan='\033[1;36m'
+light_red='\033[1;31m'
+light_purple='\033[1;35m'
+yellow='\033[1;33m'
+white='\033[1;37m'
+nc='\033[0m'
+
 set -e
 
 DOTFILES_ROOT="`pwd`"
@@ -8,8 +26,8 @@ USER_HOME=`eval echo ~$USER`
 echo $USER_HOME
 for bootstrap in `find $DOTFILES_ROOT -name "bootstrap" -not -path ./*.sh` 
 do
-	echo "================================================================="
-	echo "Running $bootstrap"
-	echo "================================================================="
+	echo -e "${purple}=================================================================${nc}"
+	echo -e "${purple}Running $bootstrap${nc}"
+	echo -e "${purple}=================================================================${nc}"
 	source $bootstrap
 done
