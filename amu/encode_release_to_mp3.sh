58c9a7b71de47cb84401c5f242d2bb296179c67b
Chris O'Neil <chris.oneil@gmail.com>
2015-12-24 07:14:37 +0000

    Add util script for encoding CDs with AMU
    At the moment AMU cannot download cover images. Having to save them
    manually is costing me a lot of time, so in the mean time this script
    will pull the image down into the current directory.

---
+++ amu/encode_release_to_mp3.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+release_id=$1
+consumer_key="ujteSPrEAXsVvqTWrRiU"
+consumer_secret="XSglsnOKXACMLbhaAiVbhYVwJJbANMAV"
+present_directory=$(pwd)
+
+if [[ -z "${release_id// }" ]]; then
+    echo "A release ID must be supplied."
+    exit 1
+fi
+
+function get_image()
+{
+    primary_image_url=$(curl https://api.discogs.com/releases/$release_id --user-agent "amu/0.1" -H "Authorization: Discogs key=ujteSPrEAXsVvqTWrRiU, secret=XSglsnOKXACMLbhaAiVbhYVwJJbANMAV"  | jq '.images | map(select(.type == "primary")) | .[]["resource_url"]')
+    # curl wouldn't download the image without the quotes removed.
+    url_without_surrounding_quotes="${primary_image_url%\"}"
+    url_without_surrounding_quotes="${url_without_surrounding_quotes#\"}"
+    curl $url_without_surrounding_quotes >> cover.jpg
+}
+
+function rip_cd()
+{
+    amu rip
+    amu encode wav mp3 --keep-source --discogs-id=$release_id
+}
+
+#[[ -d "$release_id" ]] && rm -rf "$release_id"
+#mkdir "$release_id"
+cd "$release_id"
+get_image
+rip_cd
+get_image # The image is moved in the encoding process, so re-download it.
+cd $present_directory
