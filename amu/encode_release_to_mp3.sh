82e7e1b8b3088cdd81988af1f101be34a7532318
Chris O'Neil <chris.oneil@gmail.com>
2015-12-24 08:18:11 +0000

    Case for release with a single image

--- amu/encode_release_to_mp3.sh
+++ amu/encode_release_to_mp3.sh
@@ -13,10 +13,18 @@ fi
 function get_image()
 {
     primary_image_url=$(curl https://api.discogs.com/releases/$release_id --user-agent "amu/0.1" -H "Authorization: Discogs key=ujteSPrEAXsVvqTWrRiU, secret=XSglsnOKXACMLbhaAiVbhYVwJJbANMAV"  | jq '.images | map(select(.type == "primary")) | .[]["resource_url"]')
-    # curl wouldn't download the image without the quotes removed.
-    url_without_surrounding_quotes="${primary_image_url%\"}"
-    url_without_surrounding_quotes="${url_without_surrounding_quotes#\"}"
-    curl $url_without_surrounding_quotes >> cover.jpg
+    if [[ ! -z $primary_image_url ]]; then
+        primary_image_url=$(curl https://api.discogs.com/releases/$release_id --user-agent "amu/0.1" -H "Authorization: Discogs key=ujteSPrEAXsVvqTWrRiU, secret=XSglsnOKXACMLbhaAiVbhYVwJJbANMAV"  | jq '.images | map(select(.type == "primary")) | .[]["resource_url"]')
+        # curl wouldn't download the image without the quotes removed.
+        url_without_surrounding_quotes="${primary_image_url%\"}"
+        url_without_surrounding_quotes="${url_without_surrounding_quotes#\"}"
+        curl $url_without_surrounding_quotes >> cover.jpg
+    else
+        secondary_image_url=$(curl https://api.discogs.com/releases/$release_id --user-agent "amu/0.1" -H "Authorization: Discogs key=ujteSPrEAXsVvqTWrRiU, secret=XSglsnOKXACMLbhaAiVbhYVwJJbANMAV"  | jq '.images | .[]["resource_url"]')
+        url_without_surrounding_quotes="${secondary_image_url%\"}"
+        url_without_surrounding_quotes="${url_without_surrounding_quotes#\"}"
+        curl $url_without_surrounding_quotes >> cover.jpg
+    fi
 }
 
 function rip_cd()
@@ -25,8 +33,8 @@ function rip_cd()
     amu encode wav mp3 --keep-source --discogs-id=$release_id
 }
 
-#[[ -d "$release_id" ]] && rm -rf "$release_id"
-#mkdir "$release_id"
+[[ -d "$release_id" ]] && rm -rf "$release_id"
+mkdir "$release_id"
 cd "$release_id"
 get_image
 rip_cd
