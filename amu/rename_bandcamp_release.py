7955d66d77c600acd4c81389ea158b05acf56ba6
Chris O'Neil <chris.oneil@gmail.com>
2015-12-28 03:15:53 +0000

    Scripts for encoding a bandcamp release to mp3

---
+++ amu/rename_bandcamp_release.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+import os
+
+def main():
+    path = os.getcwd().decode('utf-8')
+    for flac_file in [f for f in os.listdir(path) if f.endswith('.flac')]:
+        start = flac_file.rfind('-')
+        start += 2
+        destination_file = flac_file[start:].replace(' ', '_')
+        os.rename(os.path.join(path, flac_file), os.path.join(path, destination_file))
+
+if __name__ == '__main__':
+    sys.exit(main())
