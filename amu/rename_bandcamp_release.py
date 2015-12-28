7c68caf872b383449f80eeb7e905a7f010df84f0
Chris O'Neil <chris.oneil@gmail.com>
2015-12-28 04:44:17 +0000

    Rename covers

--- amu/rename_bandcamp_release.py
+++ amu/rename_bandcamp_release.py
@@ -4,13 +4,27 @@ import os
 
 def main():
     path = os.getcwd().decode('utf-8')
-    for flac_file in [f for f in os.listdir(path) if f.endswith('.flac')]:
+    rename_flacs(path)
+    rename_covers(path)
+
+def rename_flacs(current_directory):
+    for flac_file in [f for f in os.listdir(current_directory) if f.endswith('.flac')]:
         start = flac_file.rfind('-')
         start += 2
-        destination_file = flac_file[start:].replace(' ', '_')
-        destination_file = destination_file.replace("'", '_')
-        destination_file = destination_file.replace('"', '_')
-        os.rename(os.path.join(path, flac_file), os.path.join(path, destination_file))
+        rename_file(current_directory, flac_file, start)
+
+def rename_covers(current_directory):
+    for cover in [f for f in os.listdir(current_directory) if f.endswith(('.png', '.jpg'))]:
+        start = cover.rfind('-')
+        if start != -1:
+            start += 2
+            rename_file(current_directory, cover, start)
+
+def rename_file(folder, source_file, start):
+    destination_file = source_file[start:].replace(' ', '_')
+    destination_file = destination_file.replace("'", '_')
+    destination_file = destination_file.replace('"', '_')
+    os.rename(os.path.join(folder, source_file), os.path.join(folder, destination_file))
 
 if __name__ == '__main__':
     sys.exit(main())
