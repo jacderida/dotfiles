a8ac247205b45fb1ce673cf64562911959e05e1b
Chris O'Neil <chris.oneil@gmail.com>
2017-06-15 16:08:59 +0100

    Simple script for bootstrapping Windows

---
+++ bootstrap.bat
@@ -0,0 +1,7 @@
+set dotfiles=%USERPROFILE%\dev\dotfiles
+
+if not exist %USERPROFILE%\.local\bin mkdir -p %USERPROFILE%\.local\bin
+if not exist %USERPROFILE%\.gitconfig mklink %USERPROFILE%\.gitconfig %dotfiles%\git\.gitconfig
+if not exist %USERPROFILE%\.githelpers mklink %USERPROFILE%\.gitconfig %dotfiles%\git\.githelpers
+if not exist %USERPROFILE%\.local\bin\git_diff_wrapper mklink %USERPROFILE%\.local\bin\git_diff_wrapper %dotfiles%\git\git_diff_wrapper
+if not exist %USERPROFILE%\.bashrc mklink %USERPROFILE%\.bashrc %dotfiles%\bash\.bashrc
