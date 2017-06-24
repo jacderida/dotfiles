3226fcc7101d15ae2c3ef636d8948f96345bae8a
Chris O'Neil <chris.oneil@gmail.com>
2017-06-24 20:56:13 +0100

    Settings for ConEmu

--- bootstrap.bat
+++ bootstrap.bat
@@ -5,3 +5,7 @@ if not exist %USERPROFILE%\.gitconfig mklink %USERPROFILE%\.gitconfig %dotfiles%
 if not exist %USERPROFILE%\.githelpers mklink %USERPROFILE%\.githelpers %dotfiles%\git\.githelpers
 if not exist %USERPROFILE%\.local\bin\git_diff_wrapper mklink %USERPROFILE%\.local\bin\git_diff_wrapper %dotfiles%\git\git_diff_wrapper
 if not exist %USERPROFILE%\.bashrc mklink %USERPROFILE%\.bashrc %dotfiles%\bash\.bashrc
+if exist %USERPROFILE%\AppData\Roaming\ConEmu.xml (
+   del %USERPROFILE%\AppData\Roaming\ConEmu.xml
+   mklink %USERPROFILE%\AppData\Roaming\ConEmu.xml %dotfiles%\conemu\ConEmu.xml
+)
