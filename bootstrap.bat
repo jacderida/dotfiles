7d48f4f1a7e8e3548b7552f5d224a24c2ca58ce9
Chris O'Neil <chris.oneil@gmail.com>
2017-06-24 22:12:44 +0100

    Change process to remove and re-link files

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,11 +1,14 @@
 set dotfiles=%USERPROFILE%\dev\dotfiles
 
 if not exist %USERPROFILE%\.local\bin mkdir -p %USERPROFILE%\.local\bin
-if not exist %USERPROFILE%\.gitconfig mklink %USERPROFILE%\.gitconfig %dotfiles%\git\.gitconfig
-if not exist %USERPROFILE%\.githelpers mklink %USERPROFILE%\.githelpers %dotfiles%\git\.githelpers
-if not exist %USERPROFILE%\.local\bin\git_diff_wrapper mklink %USERPROFILE%\.local\bin\git_diff_wrapper %dotfiles%\git\git_diff_wrapper
-if not exist %USERPROFILE%\.bashrc mklink %USERPROFILE%\.bashrc %dotfiles%\bash\.bashrc
-if exist %USERPROFILE%\AppData\Roaming\ConEmu.xml (
-   del %USERPROFILE%\AppData\Roaming\ConEmu.xml
-   mklink %USERPROFILE%\AppData\Roaming\ConEmu.xml %dotfiles%\conemu\ConEmu.xml
-)
+
+if exist %USERPROFILE%\.gitconfig del %USERPROFILE%\.gitconfig
+mklink %USERPROFILE%\.gitconfig %dotfiles%\git\.gitconfig
+if exist %USERPROFILE%\.githelpers del %USERPROFILE%\.githelpers
+mklink %USERPROFILE%\.githelpers %dotfiles%\git\.githelpers
+if exist %USERPROFILE%\.local\bin\git_diff_wrapper del %USERPROFILE%\.local\bin\git_diff_wrapper
+mklink %USERPROFILE%\.local\bin\git_diff_wrapper %dotfiles%\git\git_diff_wrapper
+if exist %USERPROFILE%\.bashrc del %USERPROFILE%\.bashrc
+mklink %USERPROFILE%\.bashrc %dotfiles%\bash\.bashrc
+if exist %USERPROFILE%\AppData\Roaming\ConEmu.xml del %USERPROFILE%\AppData\Roaming\ConEmu.xml
+mklink %USERPROFILE%\AppData\Roaming\ConEmu.xml %dotfiles%\conemu\ConEmu.xml
