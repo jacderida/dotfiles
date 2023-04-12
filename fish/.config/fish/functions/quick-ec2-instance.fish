a65d7afbc11e315526df4f84ae086ec9eb98aadd
Chris O'Neil <chriso83@protonmail.com>
2023-04-12 17:41:46 +0100

    Extend quick-ec2-instance script
    Select the subnet, security group and keypair based on user input.
    
    There is duplication between the subnet and security group selection, but I couldn't get it to work
    correctly using a function.

--- fish/.config/fish/functions/quick-ec2-instance.fish
+++ fish/.config/fish/functions/quick-ec2-instance.fish
@@ -3,24 +3,59 @@ function quick-ec2-instance
     rm -f ~/.aws/quick-instance-id
 
     set image_id $AWS_QUICK_EC2_INSTANCE_IMAGE_ID
-    set subnet_id $AWS_QUICK_EC2_INSTANCE_SUBNET_ID
-    set security_group_id $AWS_QUICK_EC2_INSTANCE_SG_ID
     set keypair $AWS_QUICK_EC2_INSTANCE_KEYPAIR
     set instance_type $AWS_QUICK_EC2_INSTANCE_TYPE
+
+    set subnets (aws ec2 describe-subnets \
+        --query 'Subnets[].{SubnetId: SubnetId, SubnetName: Tags[?Key==`Name`].Value | [0]}' --output text)
+    set subnet_names ""
+    for subnet in $subnets
+        set subnet_name (echo $subnet | awk '{print $2}')
+        set subnet_names $subnet_names $subnet_name
+    end
+
+    set selected_subnet_name (echo $subnet_names | tr ' ' '\n' | fzf)
+    for subnet in $subnets
+        set subnet_id (echo $subnet | awk '{print $1}')
+        set subnet_name (echo $subnet | awk '{print $2}')
+        if test "$selected_subnet_name" = "$subnet_name"
+            break
+        end
+    end
+
+    set security_groups (aws ec2 describe-security-groups \
+        --query 'SecurityGroups[].{GroupId: GroupId, GroupName: GroupName}' --output text)
+    set sg_names ""
+    for sg in $security_groups
+        set sg_name (echo $sg | awk '{print $2}')
+        set sg_names $sg_names $sg_name
+    end
+
+    set selected_sg_name (echo $sg_names | tr ' ' '\n' | fzf)
+    for sg in $security_groups
+        set sg_id (echo $sg | awk '{print $1}')
+        set sg_name (echo $sg | awk '{print $2}')
+        if test "$selected_sg_name" = "$sg_name"
+            break
+        end
+    end
+
+    set keypairs (aws ec2 describe-key-pairs --query 'KeyPairs[].KeyName' --output text)
+    set keypair (echo $keypairs | awk '{for (i = 1; i <= NF; i++) print $i}' | fzf)
 
     echo "======================"
     echo "= Launching instance ="
     echo "======================"
     echo "AMI: $image_id"
     echo "Subnet: $subnet_id"
-    echo "Security group ID: $security_group_id"
+    echo "Security group ID: $sg_id"
     echo "Keypair name: $keypair"
     echo "Instance type: $instance_type"
 
     set launched_instance_id (aws ec2 run-instances \
         --image-id $image_id \
         --subnet-id $subnet_id \
-        --security-group-ids $security_group_id \
+        --security-group-ids $sg_id \
         --instance-type $instance_type \
         --key-name $keypair \
         --query 'Instances[0].InstanceId' \
@@ -44,13 +79,13 @@ function quick-ec2-instance
         --instance-ids $launched_instance_id \
         --query 'Reservations[0].Instances[0].PublicIpAddress' \
         --output text)
-    ssh -i ~/.ssh/$keypair ubuntu@$public_ip "bash --version"
+    ssh -oStrictHostKeyChecking=no ubuntu@$public_ip "bash --version"
     while test $status -ne 0
         echo "SSH still not ready. Will query again in 5 seconds."
         sleep 5
-        ssh -i ~/.ssh/$keypair -oStrictHostKeyChecking=no ubuntu@$public_ip "bash --version"
+        ssh -oStrictHostKeyChecking=no ubuntu@$public_ip "bash --version"
     end
 
-    echo "ssh to the instance with: ssh -i ~/.ssh/$keypair ubuntu@$public_ip"
-    echo "ssh -i ~/.ssh/$keypair ubuntu@$public_ip" | xclip -selection clipboard
+    echo "ssh to the instance with: ssh ubuntu@$public_ip"
+    echo "ssh ubuntu@$public_ip" | wl-copy
 end
